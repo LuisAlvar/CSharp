1a64f77729d844954467a9695cb613bd63188d2d
luis <alvarez.en.luis@gmail.com>
2025-02-06 06:40:23 -0800

    logger functionality is tested

--- ExecutePowerShellConsole/IIS/PowerShell.cs
+++ ExecutePowerShellConsole/IIS/PowerShell.cs
@@ -7,40 +7,52 @@ namespace ExecutePowerShellConsole.IIS
   {
     public static void Execute()
     {
-      // Define your PowerShell script as a string
-      ProcessStartInfo scriptInfo = new ProcessStartInfo("powershell.exe", string.Join(" ", "-NoProfile", "-C", IIS.Scripts.AddNewWebsiteToIIS))
+      try
       {
-        UseShellExecute = false,
-        RedirectStandardOutput = true,
-        RedirectStandardError = true,
-        CreateNoWindow = true
-      };
-
-      using (Process process = new Process())
-      {
-        process.StartInfo = scriptInfo;
-        process.Start();
-
-        // Read the output
-        string output = process.StandardOutput.ReadToEnd();
-        string error = process.StandardError.ReadToEnd();
-
-        process.WaitForExit();
-
-        //Display the script
-        Console.WriteLine("Script:");
-        Console.WriteLine(IIS.Scripts.AddNewWebsiteToIIS);
-
-        // Display the output and error (if any)
-        Console.WriteLine("Output:");
-        Console.WriteLine(output);
-
-        if (!string.IsNullOrEmpty(error))
+        Logger.AddMessage("Executing Powershell script ....");
+        // Define your PowerShell script as a string
+        ProcessStartInfo scriptInfo = new ProcessStartInfo("powershell.exe", string.Join(" ", "-NoProfile", "-C", IIS.Scripts.AddNewWebsiteToIIS))
         {
-          Console.WriteLine("Error:");
-          Console.WriteLine(error);
+          UseShellExecute = false,
+          RedirectStandardOutput = true,
+          RedirectStandardError = true,
+          CreateNoWindow = true
+        };
+
+        using (Process process = new Process())
+        {
+          process.StartInfo = scriptInfo;
+          process.Start();
+
+          // Read the output
+          string output = process.StandardOutput.ReadToEnd();
+          string error = process.StandardError.ReadToEnd();
+
+          process.WaitForExit();
+
+          //Display the script
+          Console.WriteLine("Script:");
+          Console.WriteLine(IIS.Scripts.AddNewWebsiteToIIS);
+
+          // Display the output and error (if any)
+          Console.WriteLine("Output:");
+          Console.WriteLine(output);
+
+          if (!string.IsNullOrEmpty(error))
+          {
+            Console.WriteLine("Error:");
+            Console.WriteLine(error);
+          }
         }
+
+        Logger.AddMessage("At the end of executing Powershell script ....");
       }
+      catch (Exception ex)
+      {
+        Logger.AddMessage($"Failed to execute {ex}", LoggerMessageType.ERROR);
+        Logger.Flush();
+      }
+ 
     }
   }
 }
--- ExecutePowerShellConsole/Logger.cs
+++ ExecutePowerShellConsole/Logger.cs
@@ -1,37 +1,103 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Security.AccessControl;
+using System.Security.Principal;
 
 namespace ExecutePowerShellConsole
 {
   public class Logger
   {
-    private const string OutputFileDir = @"C:\";
+    private const string OutputFileDir = @"C:/Program Files/LeafSoft/HealthCheckApp";
     private const string OutputFileName = "Install_Logs.txt";
 
     private static List<string> lsLogContainer = new List<string>();
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="type"></param>
     public static void AddMessage(string message, LoggerMessageType type = LoggerMessageType.INFO)
     {
       lsLogContainer.Add($"{type.ToString()}: {DateTime.UtcNow.ToString()}: {message}");
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
     public static void Flush()
     {
+      int retryCount = 5;
+      int delay = 1000;
+
+      if (lsLogContainer.Count == 0) return;
       if (!File.Exists(Path.Combine(OutputFileDir, OutputFileName)))
       {
-        Directory.CreateDirectory(OutputFileDir);
-        File.Create(Path.Combine(OutputFileDir, OutputFileName));
+        EnsureAndTakeOwnership(OutputFileDir);
+        FileStream fs = File.Create(Path.Combine(OutputFileDir, OutputFileName));
+        fs.Close();
       }
-      using (StreamWriter write = new StreamWriter(Path.Combine(OutputFileDir, OutputFileName)))
+      while (retryCount > 0)
       {
-        foreach (var item in lsLogContainer)
+        try
         {
-          write.WriteLine(item);
+          using (StreamWriter write = new StreamWriter(Path.Combine(OutputFileDir, OutputFileName)))
+          {
+            foreach (var item in lsLogContainer)
+            {
+              write.WriteLine(item);
+            }
+          }
+          Console.WriteLine("File accessed and written successfully.");
+          lock (lsLogContainer)
+          {
+            lsLogContainer = new List<string>();
+          }
+          break;
         }
+        catch (IOException ex) when (IsFileLocked(ex))
+        {
+          Console.WriteLine($"File is being used by another process. Retry in {delay/1000} seconds ...");
+          System.Threading.Thread.Sleep(delay);
+        }
+        --retryCount;
       }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="ex"></param>
+    /// <returns></returns>
+    private static bool IsFileLocked(IOException ex)
+    {
+      int errorCode = System.Runtime.InteropServices.Marshal.GetHRForException(ex) & ((1 << 16) - 1);
+      return errorCode == 32 | errorCode == 33; // ERROR_SHARING_VIOLATION or ERROR_LOCK_VIOLATION
+    }
+
+    private static void EnsureAndTakeOwnership(string folderPath)
+    {
+      // Create the folder if it doesnt exist
+      if (!Directory.Exists(folderPath))
+      {
+        Directory.CreateDirectory($"{folderPath}");
+      }
+
+      // Get the current user
+      var currentUser = WindowsIdentity.GetCurrent();
+      var ntAccount = new NTAccount(currentUser.Name);
+
+      // Get the security descriptor of the folder
+      DirectoryInfo di = new DirectoryInfo(folderPath);
+      DirectorySecurity ds = di.GetAccessControl();
+
+      // Set the owner to the current user
+      ds.SetOwner(ntAccount);
+
+      // Apply the changes
+      di.SetAccessControl(ds);
+
+      Console.WriteLine($"Ownership of folder '{folderPath}' has been take by {currentUser.Name}");
     }
   }
 
